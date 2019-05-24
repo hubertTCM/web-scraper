9caad1380544a9dc86fb8e33dd22f4a142aeb69b
Hubert Fu <fuqunxing@gmail.com>
2019-05-25 03:17:53 +1200

    correct script

--- QiHuangShuYuan/downloader.py
+++ QiHuangShuYuan/downloader.py
@@ -10,8 +10,13 @@ Entry = collections.namedtuple('Entry', 'name url')
 def query_data(url):
     try:
         with urllib.request.urlopen(url) as response:
-            return response.read().decode('utf8')
-    except:
+            data = response.read()
+            try:
+                return data.decode('utf8')
+            except:
+                return data
+    except Exception as e:
+        print('exception in url ****', e, url)
         return ""
 
 # 医案链接
@@ -34,8 +39,41 @@ def parse_summary(entry, parent_directory):
                 details.append(Entry(name= link.get_text(), url=link['href']))
     
     for item in details:
-        print(item.name)
-        print(item.url)
+        download_detail_pdf(item, entry_directory)
+
+
+# 医案pdf
+def download_detail_pdf(entry, parent_directory):
+    entry_directory = os.path.join(parent_directory, entry.name)
+    if not os.path.exists(entry_directory):
+        os.makedirs(entry_directory)
+    raw_text = query_data('http://www.xlgyyjs.com' + entry.url)
+    with open(os.path.join(entry_directory, "content.html"), "w+") as content_file:
+        content_file.write(raw_text)
+    
+    bs = BeautifulSoup(raw_text, 'html.parser')
+    nameList = bs.find_all('div', {'class':'margin_a8'})
+
+    downloaded = []
+    for name in nameList:
+        links = name.find_all('a')
+        for link in links:
+            url = link['href'].strip()
+            if not url.startswith("http"):
+                url = 'http://www.xlgyyjs.com' + url
+            print(url)
+
+            text = link.get_text().strip()
+            if text.lower().endswith('pdf') or url.lower().endswith('pdf'):
+                with open(os.path.join(entry_directory, text), "w+") as content_file:
+                    data = query_data(url)
+                    if data:
+                        content_file.write(data)
+                    else:
+                        print(text, url)
+            else:
+                pass
+
 
 
 if __name__ == "__main__":
